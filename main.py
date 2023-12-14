aeb507343d7b3282e52910df2618a9d8d57218cb
Andrea Miele <andrea.miele.pro@gmail.com>
2023-12-14 13:58:21 +0100

    main with mask

--- main.py
+++ main.py
@@ -250,10 +250,10 @@ class Tokenizer(object):
     
 dict_path = "/home/ml4science2023/GAN/sketch/token_dict.pkl"
 
-sketch=load_sketch_data("/home/ml4science2023/GAN/sketch/quickdraw/The Eiffel Tower.npz", 'train')
-sketch2=load_sketch_data("/home/ml4science2023/GAN/sketch/quickdraw/church.npz", 'train')
-sketch3=load_sketch_data("/home/ml4science2023/GAN/sketch/quickdraw/skull.npz", 'train')
-sketch4=load_sketch_data("/home/ml4science2023/GAN/sketch/quickdraw/sun.npz", 'train')
+sketch=load_sketch_data("/home/ml4science2023/GAN/sketch/quickdraw_dataset/The Eiffel Tower.npz", 'train')
+sketch2=load_sketch_data("/home/ml4science2023/GAN/sketch/quickdraw_dataset/church.npz", 'train')
+sketch3=load_sketch_data("/home/ml4science2023/GAN/sketch/quickdraw_dataset/skull.npz", 'train')
+sketch4=load_sketch_data("/home/ml4science2023/GAN/sketch/quickdraw_dataset/sun.npz", 'train')
 result1 = np.concatenate((sketch, sketch2), axis=0)
 result2 = np.concatenate((result1, sketch3), axis=0)
 sketches = np.concatenate((result2, sketch3), axis=0)
@@ -287,8 +287,8 @@ class SketchDataset(Dataset):
     
 dataset = SketchDataset(train_sketches, train_labels)
 dataset_test = SketchDataset(test_sketches, test_labels)
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
-dataloader_test = DataLoader(dataset_test, batch_size=64, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+dataloader_test = DataLoader(dataset_test, batch_size=32, shuffle=True)
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=5000):
@@ -304,23 +304,20 @@ class PositionalEncoding(nn.Module):
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        # x.size(1) should match the sequence length dimension
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
-
 
 class SketchEmbedding(nn.Module):
     def __init__(self, vocab_size, d_model, max_seq_length):
         super(SketchEmbedding, self).__init__()
         self.token_embedding = nn.Embedding(vocab_size, d_model)
         self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
-
     def forward(self, sketch_tokens):
-        # Convert sketch_tokens to long tensor if not already
         sketch_tokens = sketch_tokens.long()  # Ensure input is of type Long
         token_embeddings = self.token_embedding(sketch_tokens)  # (batch_size, seq_length, d_model)
+        token_embeddings = token_embeddings.squeeze(2)  # Remove the dimension with size 1
+        token_embeddings = token_embeddings.transpose(0, 1)  # Transpose to get [seq_len, batch_size, d_model]
         return self.positional_encoding(token_embeddings)
-
 
 class SelfAttentionBottleneck(nn.Module):
     def __init__(self, embed_size, heads):
@@ -370,23 +367,25 @@ class Sketchformer(nn.Module):
       # Convert src and tgt to long type if they are not already
       src = src.long()
       tgt = tgt.long()
-
       # Create masks for padding tokens
       PAD_IDX = 0
-      src_key_padding_mask = (src == PAD_IDX)
-      tgt_key_padding_mask = (tgt == PAD_IDX)
-
+      # Create a source and target key padding mask with shape [batch_size, sequence_length]
+      src_key_padding_mask = (src == PAD_IDX)  # Transpose to get shape [batch_size, seq_len]
+      tgt_key_padding_mask = (tgt == PAD_IDX)  # Transpose to get shape [batch_size, seq_len]
       # Create a causal mask for the target, preventing attention to future tokens
       tgt_mask = torch.triu(torch.ones((tgt.size(1), tgt.size(1)), device=tgt.device).bool(), diagonal=1)
 
       # Pass through the sketch embedding layer
       src_emb = self.sketch_embedding(src) * torch.sqrt(torch.tensor(self.d_model, dtype=torch.float32))
       tgt_emb = self.sketch_embedding(tgt) * torch.sqrt(torch.tensor(self.d_model, dtype=torch.float32))
-
+      
       # Apply positional encoding
-      src_emb = self.pos_encoder(src_emb.transpose(0, 1))
-      tgt_emb = self.pos_encoder(tgt_emb.transpose(0, 1))
+      # Ensure src_emb and tgt_emb are 3D tensors with correct shape [sequence_length, batch_size, embedding_dimension]
+      src_emb = src_emb.squeeze(2).transpose(0, 1)  # Remove extra dimension and transpose
+      tgt_emb = tgt_emb.squeeze(2).transpose(0, 1)  # Remove extra dimension and transpose
 
+      src_emb = self.pos_encoder(src_emb)
+      tgt_emb = self.pos_encoder(tgt_emb)
       # Pass through the encoder and decoder
       memory = self.encoder(src_emb, mask=src_mask, src_key_padding_mask=src_key_padding_mask)
       output = self.decoder(tgt_emb, memory, tgt_mask=tgt_mask,
@@ -410,7 +409,7 @@ if torch.cuda.is_available():
 else:
     print("CUDA (GPU support) is not available. Using CPU...")
     device = torch.device("cpu")
-    
+PAD_IDX=0    
 
 
 def load_config(config_path):
@@ -428,10 +427,6 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 scheduler2 = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
 
-
-from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter('runs/experiment_name')
-# Training Loop
 num_epochs = 30  # Number of epochs
 SOS_token = 1002
 
@@ -440,62 +435,79 @@ for epoch in range(num_epochs):
     model.train()  # Set the model to training mode
     train_loss = 0.0
 
-    # Training loop
     for i, (sketches, _) in enumerate(dataloader):
+        sketches = sketches.squeeze(2) 
         sketches = sketches.to(device)
-        sos_tensor = torch.full((sketches.size(0), 1, sketches.size(2)), SOS_token, dtype=torch.long, device=device)
-        tgt_input = torch.cat([sos_tensor, sketches[:, :-1, :]], dim=1)
+        sos_tensor = torch.full((sketches.size(0), 1), SOS_token, dtype=torch.long, device=device)
+        tgt_input = torch.cat([sos_tensor, sketches[:, :-1]], dim=1)
 
-        # Create masks
+    # Create masks for source and target sequences
         src_mask = (sketches != PAD_IDX).unsqueeze(-2)
         tgt_mask = (tgt_input != PAD_IDX).unsqueeze(-2)
-        tgt_mask = tgt_mask & torch.triu(torch.ones((tgt_input.size(0), tgt_input.size(1), tgt_input.size(1)), device=device), diagonal=1).bool()
 
-        # Forward pass with masks
-        outputs = model(sketches, tgt_input, src_key_padding_mask=~src_mask, tgt_key_padding_mask=~tgt_mask)
+    # Correct key padding masks to have shape [batch_size, sequence_length]
+        src_key_padding_mask = (sketches == PAD_IDX).transpose(0,1)
+        tgt_key_padding_mask = (tgt_input == PAD_IDX).transpose(0,1)
+
+    # Create a causal mask for the target
+        sequence_length = tgt_input.size(1)
+        upper_triangular_mask = torch.triu(torch.ones((sequence_length, sequence_length), device=device), diagonal=1).bool()
+        tgt_mask = tgt_mask & upper_triangular_mask
+
+    # Forward pass with masks
+        outputs = model(sketches, tgt_input, src_key_padding_mask=src_key_padding_mask, tgt_key_padding_mask=tgt_key_padding_mask)
         outputs = outputs.view(-1, outputs.size(-1))
         targets = sketches.view(-1)
         loss = criterion(outputs, targets)
-
         # Backward pass and optimize
         optimizer.zero_grad()
         loss.backward()
         nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
         print(f"{i/len(dataloader)*100} % | Loss: {loss.item():.4f}")
-        for name, param in model.named_parameters():
-              writer.add_histogram('gradients/' + name, param.grad, epoch * len(dataloader) + i)
 
         optimizer.step()
 
         train_loss += loss.item()
         # Log training information
-        writer.add_scalar('Loss/train', loss.item(), epoch * len(dataloader) + i)
 
     # Calculate average training loss
     avg_train_loss = train_loss / len(dataloader)
 
-    # Validation loop
     model.eval()  # Set the model to evaluation mode
     val_loss = 0.0
     with torch.no_grad():
         for sketches, _ in dataloader_test:
+            sketches = sketches.squeeze(2)
             sketches = sketches.to(device)
-            sos_tensor = torch.full((sketches.size(0), 1, sketches.size(2)), SOS_token, dtype=torch.long, device=device)
-            tgt_input = torch.cat([sos_tensor, sketches[:, :-1, :]], dim=1)
+            sos_tensor = torch.full((sketches.size(0), 1), SOS_token, dtype=torch.long, device=device)
+            tgt_input = torch.cat([sos_tensor, sketches[:, :-1]], dim=1)
 
-            # Forward pass
-            outputs = model(sketches, tgt_input)
+    # Create masks for source and target sequences
+            src_mask = (sketches != PAD_IDX).unsqueeze(-2)
+            tgt_mask = (tgt_input != PAD_IDX).unsqueeze(-2)
+
+    # Correct key padding masks to have shape [batch_size, sequence_length]
+            src_key_padding_mask = (sketches == PAD_IDX).transpose(0,1)
+            tgt_key_padding_mask = (tgt_input == PAD_IDX).transpose(0,1)
+
+    # Create a causal mask for the target
+            sequence_length = tgt_input.size(1)
+            upper_triangular_mask = torch.triu(torch.ones((sequence_length, sequence_length), device=device), diagonal=1).bool()
+            tgt_mask = tgt_mask & upper_triangular_mask
+
+    # Forward pass with masks
+            outputs = model(sketches, tgt_input, src_key_padding_mask=src_key_padding_mask, tgt_key_padding_mask=tgt_key_padding_mask)
             outputs = outputs.view(-1, outputs.size(-1))
             targets = sketches.view(-1)
             loss = criterion(outputs, targets)
-
             val_loss += loss.item()
-
     # Calculate average validation loss
     avg_val_loss = val_loss / len(dataloader_test)
-    writer.add_scalar('Loss/validation', avg_val_loss, epoch)
 
     # Update learning rate scheduler
     scheduler2.step(avg_val_loss)
+    model_save_path = os.path.join('saved_models', f'model_epoch_{epoch+1}.pth')
+    torch.save(model.state_dict(), model_save_path)
 
+    print("model saved....")
     print(f'Epoch [{epoch+1}/{num_epochs}], Training Loss: {avg_train_loss:.4f}, Validation Loss: {avg_val_loss:.4f}')
